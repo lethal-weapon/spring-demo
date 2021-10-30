307c8ef2c0d08601a3b6a4cbe04917f57e5c8421
Sean Kramer <sean@kramerdesigns.app>
2021-10-30 18:01:19 +0800

    [Employment][Sean]refactor: add repository annotation on 2 repository interfaces

--- src/main/java/com/example/springdemo/data/CompanyRepository.java
+++ src/main/java/com/example/springdemo/data/CompanyRepository.java
@@ -2,7 +2,9 @@ package com.example.springdemo.data;
 
 import com.example.springdemo.domain.Company;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository
          extends PagingAndSortingRepository<Company, Long> {
 
--- src/main/java/com/example/springdemo/data/EmployeeRepository.java
+++ src/main/java/com/example/springdemo/data/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.example.springdemo.data;
 
 import com.example.springdemo.domain.Employee;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository
          extends PagingAndSortingRepository<Employee, Long> {
 
